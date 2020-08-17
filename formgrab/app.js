ee1c4edfe0af8065b317fa362e4f6687d6d42970
VMW-DEV-DEBIAN10 <vmw-dev-debian10@debian.debian>
2020-08-17 04:09:10 -0400

    use multer read text form data

--- formgrab/app.js
+++ formgrab/app.js
@@ -1,19 +1,23 @@
 var express = require('express')
-var bodyParser = require('body-parser')
- 
+var multer = require('multer')
+
 var app = express()
+
+app.use(multer().array())
  
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
  
 app.get('/', (req,res) => {
   	res.sendFile(__dirname + "/form.html")
 })
 
 // POST /login gets urlencoded bodies
-app.post('/upload', urlencodedParser, function (req, res) {
-    console.log(req.body)
-    res.send('uploading, ' + req.body.upload)
+app.post('/upload', function (req, res) {
+    console.log(req.body.upload)
+    console.log(req.body.myname)
+    console.log(req.files)
+    res.send('Hi' + req.body.myname  +  ' uploading, ' + req.body.upload)
 })
 
 app.listen(3000, () => {
