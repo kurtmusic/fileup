6d39f7bb802d5183092e09bc9983f1269869d42a
Kurt Ley <leymku.rt@gmail.com>
2020-08-17 16:19:29 +0800

    start multer file upload functionality

--- formgrab/app.js
+++ formgrab/app.js
@@ -1,23 +1,22 @@
 var express = require('express')
 var multer = require('multer')
-
+var upload = multer({dest: 'uploads/'})
+// include body parser for non multipart/form-data
 var app = express()
 
-app.use(multer().array())
+//app.use(multer().array()) use it for every route (not this)
  
 // create application/x-www-form-urlencoded parser
 
- 
 app.get('/', (req,res) => {
   	res.sendFile(__dirname + "/form.html")
 })
 
 // POST /login gets urlencoded bodies
-app.post('/upload', function (req, res) {
-    console.log(req.body.upload)
+app.post('/upload', upload.single('upload'), function (req, res) {
     console.log(req.body.myname)
-    console.log(req.files)
-    res.send('Hi' + req.body.myname  +  ' uploading, ' + req.body.upload)
+    console.log(req.file)
+    res.send('Hi' + req.body.myname  +  ' uploading, ' + req.file)
 })
 
 app.listen(3000, () => {
